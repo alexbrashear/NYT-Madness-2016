03d83d01c74f5c4fc8076314f9aea43b35a5eb34
alexbrashear <alexjbrashear@gmail.com>
2016-03-18 14:17:06 -0400

    Team fetcher

--- NYT Madness 2016/TeamsFetcher.swift
+++ NYT Madness 2016/TeamsFetcher.swift
@@ -1,5 +1,5 @@
 //
-//  TournamentFetcher.swift
+//  TeamsFetcher.swift
 //  NYT Madness 2016
 //
 //  Created by Brashear, Alex on 3/18/16.
@@ -9,27 +9,25 @@
 import Foundation
 
 let tournamentSourceURL = "http://10.51.221.205:8080/tournament.json"
+let teamsSourceURL = "http://10.51.221.205:8080/teams.json"
 
-func fetchBrackets(completion:(Tournament?) -> ()) {
-    let URL = NSURL(string: URLString)
+func fetchTeams(completion:([Team]?) -> ()) {
+    let URL = NSURL(string: teamsSourceURL)
     let request = NSURLRequest(URL: URL!)
     
     NSURLSession.sharedSession().dataTaskWithRequest(request) { (data:NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-        let brackets = [Bracket?]()
+        var teams = [Team]()
         if let realData = data {
             do {
-                let object = try NSJSONSerialization.JSONObjectWithData(realData, options: NSJSONReadingOptions.AllowFragments) as! Array<AnyObject>
-                for bracketDictionary in object {
-                    if let bracket = parseBracket(bracketDictionary) {
-                        brackets.append(bracket)
-                    }
-                }
+                let teamsDictionary = try NSJSONSerialization.JSONObjectWithData(realData, options: NSJSONReadingOptions.AllowFragments) as! Dictionary<String, Int>
+                let teamParser = TeamParser()
+                teams = teamParser.parseTeams(teamsDictionary)
                 
             }catch {
                 
             }
         }
-        completion(brackets)
+        completion(teams)
         
         }.resume()
 }
