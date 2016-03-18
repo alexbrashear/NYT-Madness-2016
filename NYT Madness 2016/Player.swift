a8989db0ac91a67de0bbbce247dfe806e2479093
alexbrashear <alexjbrashear@gmail.com>
2016-03-18 12:28:48 -0400

    Model Objects part 1

---
+++ NYT Madness 2016/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  NYT Madness 2016
+//
+//  Created by Brashear, Alex on 3/18/16.
+//  Copyright © 2016 NYTimes. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var name: String
+    var bracket: Bracket
+    var pointTotal: Int
+    
+}
---
+++ NYT Madness 2016/Tournament.swift
@@ -0,0 +1,13 @@
+//
+//  Tournament.swift
+//  NYT Madness 2016
+//
+//  Created by Brashear, Alex on 3/18/16.
+//  Copyright © 2016 NYTimes. All rights reserved.
+//
+
+import Foundation
+
+struct Tournament {
+    
+}
