0bbdc84180cc0a4dd54a0bbeed052f33870518ae
ashott <adam.shott@nytimes.com>
2016-03-18 12:55:19 -0400

    Update game object

--- NYT Madness 2016/Game.swift
+++ NYT Madness 2016/Game.swift
@@ -9,6 +9,34 @@
 import Foundation
 
 struct Game {
-    var team1: Team
-    var team2: Team
+    let teamScore1: TeamScore
+    let teamScore2: TeamScore
+    let round: TournamentRound
+    
+    var winningTeam: Team {
+        if teamScore1.score > teamScore2.score {
+            return teamScore1.team
+        }
+        return teamScore2.team
+    }
+    
+    var losingTeam: Team {
+        if teamScore1.score < teamScore2.score {
+            return teamScore1.team
+        }
+        return teamScore2.team
+    }
+    
+    var points: Int {
+        let seedDifferential = winningTeam.seed - losingTeam.seed
+        if seedDifferential > 0 {
+            return seedDifferential * round.value
+        }
+        return round.value
+    }
+}
+
+struct TeamScore {
+    let team: Team
+    let score: Int
 }
