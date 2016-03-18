1b6b5be29029086d7dbdd7817de88dcdbaef8fbf
ashott <adam.shott@nytimes.com>
2016-03-18 14:13:28 -0400

    Conflicts

--- NYT Madness 2016/TournamentFetcher.swift
+++ NYT Madness 2016/TournamentFetcher.swift
@@ -10,26 +10,26 @@ import Foundation
 
 let tournamentSourceURL = "http://10.51.221.205:8080/tournament.json"
 
-func fetchBrackets(completion:(Tournament?) -> ()) {
+func fetchBrackets(completion:([Player?]) -> ()) {
     let URL = NSURL(string: URLString)
     let request = NSURLRequest(URL: URL!)
     
     NSURLSession.sharedSession().dataTaskWithRequest(request) { (data:NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-        let brackets = [Bracket?]()
+        var players = [Player?]()
         if let realData = data {
             do {
+                let tournament = Tournament()
+                let playerParser = PlayerParser(tournament:tournament)
                 let object = try NSJSONSerialization.JSONObjectWithData(realData, options: NSJSONReadingOptions.AllowFragments) as! Array<AnyObject>
-                for bracketDictionary in object {
-                    if let bracket = parseBracket(bracketDictionary) {
-                        brackets.append(bracket)
-                    }
+                for playerDictionary in object {
+                    players.append(playerParser.parsePlayer(playerDictionary as! Dictionary<String, AnyObject>))
                 }
                 
             }catch {
                 
             }
         }
-        completion(brackets)
+        completion(players)
         
         }.resume()
 }
